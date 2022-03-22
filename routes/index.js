67fc84ae007e182f13a767501b85f0fb6726daf5
thanhloc2609 <97294495+thanhloc2609@users.noreply.github.com>
2022-03-22 13:46:57 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'test Jenkins node' });
+  res.render('index', { title: 'Hello Hô Lê' });
 });
 
 module.exports = router;
